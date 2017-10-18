bb70963b0b269016c1eff8edf32984d343c4e309
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2017-10-18 07:58:10 -0500

    Added CanvasJS

--- resources/views/PrivateGraf/PagePrivate.blade.php
+++ resources/views/PrivateGraf/PagePrivate.blade.php
@@ -1,6 +1,35 @@
+<!DOCTYPE HTML>
 <html>
-    <body>
-        <p>El canal enviado es: <span style="font-weight: bold; color: red;">
+<head>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<script type="text/javascript">
+
+window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title:{
+			text: "Pagina Privada"
+		},
+		data: [
+		{
+			// Change type to "doughnut", "line", "splineArea", etc.
+			type: "line",
+			dataPoints: [
+				{ label: "Sensor 1",  y: 45  },
+				{ label: "Sensor 2", y: 15  },
+				{ label: "Sensor 3", y: 25  },
+				{ label: "Sensor 4",  y: 30  },
+				{ label: "Sensor 5",  y: 28  }
+			]
+		}
+		]
+	});
+	chart.render();
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+<p>El canal enviado es: <span style="font-weight: bold; color: red;">
         <?php echo  $elCanal; ?></style>.
         </p>
     </body>
