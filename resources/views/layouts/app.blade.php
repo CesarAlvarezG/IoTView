519d2f79dec2ff0b66ae9016aa88b31620311402
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2017-10-19 17:13:10 -0500

    Updated app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,33 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<!doctype html>
+
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Aplición Web para el Internet de las Cosas IoT - @yield('title')</title>
+  <meta name="description" content="IoTView">
+  <meta name="Ing. César Augusto Álvarez Gaspar" content="AppIoT">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
+    @yield('headspace')
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+    @section('headerspace')
+        <header>
+            Este es el titulo de la pagina
+        </header>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+    @show
+    @section('navibar')
+        <nav>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
@@ -69,12 +63,24 @@
                     </ul>
                 </div>
             </div>
+            Esta es la barra de navegación
         </nav>
+    @show
+    @section('sidebar')
+        <aside>
+            esta es la barra lateral
+        </aside>
+    @show
 
+
+    <div class="container">
         @yield('content')
     </div>
+    @section('footerspace')
+        <footer>
+            Este es el footer
+        </footer>
+    @show
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
