3f3336a72cc3bd044b2db1499bde3af342b35a21
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2017-10-24 16:20:22 -0500

    Added User Create and User Store

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Session;
 
 class UserController extends Controller
 {
     //
+    public function create(Request $request)
+    {
+        return view('user.create');
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required | string | alpha_dash | max:66',
+            'email' => 'required | email',
+            'password' => 'required | string | min:8 | max:64',
+        ]);
+        $input = $request->all();
+        User::create($input);
+        return redirect('/home');
+    }
 }
