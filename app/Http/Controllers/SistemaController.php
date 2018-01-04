f33304c8e68b6c2882e3c49f133289c20fbe0ea1
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2018-01-04 14:53:02 -0500

    Added sistemas edit and sistemas update

--- app/Http/Controllers/SistemaController.php
+++ app/Http/Controllers/SistemaController.php
@@ -42,4 +42,33 @@ class SistemaController extends Controller
             return redirect()->back();
         }
     }
+    public function edit(Request $request, $id)
+    {
+        try{
+            $sistema=Sistema::findOrFail($id);
+            return view('sistema.edit',['data'=>$sistema]);
+        }catch(ModelNotFoundException $e)
+        {
+            Session::flash('flash_message',"El sistema ($id) no pudo ser editado");
+            return redirect()->back();
+        }
+    }
+    public function update(Request $request,$id)
+    {
+        try{
+            $sistema=Sistema::findOrFail($id);
+            $this->validate($request, [
+                'Nombre' => 'required | string ',
+                'Descripcion' => 'required | string | max:66',
+        ]);
+        $input = $request->all();
+        $sistema->fill($input)->save();
+        Session::flash('flash_message', 'Sistema exitosamente editado!');
+        return redirect('/home');
+        }catch(ModelNotFoundExcetion $e)
+        {
+            Session::flash('flash_message','El sistema ($id) no pudo ser editado');
+            return redirect()->back();
+        }
+    }
 }
---
+++ resources/views/sistema/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@section('content')
+
+
+<h1>Editar usuario</h1>
+<p class="lead">Editar este sistema.
+<a href="{{ route('sistemas.index') }}">Regresar a todos los sistemas.</a></p>
+<hr>
+
+@if($errors->any())
+    <div class="alert alert-danger">
+        @foreach($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    </div>
+@endif
+
+{!! Form::model($data, [
+    'method' => 'PUT',
+    'route' => ['sistemas.update', $data->id]
+]) !!}
+
+<div class="form-group">
+    {!! Form::label('Nombre', 'Nombre', ['class' => 'control-label']) !!}
+    {!! Form::text('Nombre', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('Descripcion', 'Dirección', ['class' => 'control-label']) !!}
+    {!! Form::text('Descripcion', null, ['class' => 'form-control']) !!}
+</div>
+    {!! Form::submit('Actualizar sistema', ['class' => 'btn btn-primary']) !!}
+    {!! Form::close() !!}
+
+@stop
