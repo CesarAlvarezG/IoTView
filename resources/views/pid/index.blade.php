7962811fc52c166edc5059845d36be01d4cc5dd8
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-01-16 16:30:25 -0500

    Interfaz grafica de PIDs index

--- resources/views/pid/index.blade.php
+++ resources/views/pid/index.blade.php
@@ -2,29 +2,85 @@
 
 
 @section('content')
+<div class="row">
+    <div class="col-xs-12">
+        <div class="Box">
+            <div class="box-header">
+                <h1 class="box-title">Listado de controles PID</h1>
+                <p><a href="{!! url('pids/create') !!}">Agregar una nuevo control PID?</a></p>
+                <div class="box-body table-responsive no-padding">
+                    <table class="table table-hover">
+                        <tr>
+                            <th>ID</th>
+                            <th>Kp</th>
+                            <th>Kd</th>
+                            <th>Ki</th>
+                            <th>Ts</th>
+                            <th>SetPoint</th>
+                            <th>Sensor</th>
+                            <th>Ver</th>
+                            <th>Editar</th>
+                            <th>Borrar</th>
+                        </tr>
+                        @foreach($list as $pid)
+                            <tr>
+                                <td>
+                                    {{ $pid->id }}
+                                </td>
+                                <td>
+                                    {{ $pid->Kp }}
+                                </td>
+                                <td>
+                                    {{ $pid->Ki }}
+                                </td>
+                                <td>
+                                    {{ $pid->Kd }}
+                                </td>
+                                <td>
+                                    {{ $pid->Ts }}
+                                </td>
+                                <td>
+                                    {{ $pid->SetPoint }}
+                                </td>
+                                <td>
+                                    {{ $pid->sensor_id }}
+                                </td>
+                                <td>
+                                    <a href="{{ route('pids.show', $pid->id) }}" class="btn btn-primary">Ver Control PID</a>
+                                </td>
+                                <td>
+                                    <a href="{{ route('pids.edit', $pid->id) }}" class="btn btn-primary">Editar Control PID</a>
+                                </td>
+                                <td>
+                                    {!! Form::open([
+                                    'method' => 'DELETE',
+                                    'route' => ['pids.destroy', $pid->id]
+                                ]) !!}
+                                {!! Form::submit('Borrar este control PID?', ['class' => 'btn btn-danger']) !!}
+                                {!! Form::close() !!}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-
-    <h1>Listado de controles PID</h1>
-    <p class="lead">Lista de todos los controles PID creados.
-    <a href="{!! url('pids/create') !!}">Agregar una nuevo control PID?</a></p>
-    <hr>
-    @foreach($list as $pid)
-        <p>Kp: {{ $pid->Kp }}</p>
-        <p>Ki: {{ $pid->Ki }}</p>
-        <p>Kd: {{ $pid->Kd }}</p>
-        <p>Ts: {{ $pid->Ts }}</p>
-        <p>SetPoint: {{ $pid->SetPoint }}</p>
-        <p>Sensor: {{ $pid->sensor_id }}</p>
-        <p>
-        <a href="{{ route('pids.show', $pid->id) }}" class="btn btn-primary">Ver control PID</a>
-        <a href="{{ route('pids.edit', $pid->id) }}" class="btn btn-primary">Editar control PID</a>
-        {!! Form::open([
-            'method' => 'DELETE',
-            'route' => ['pids.destroy', $pid->id]
-        ]) !!}
-        {!! Form::submit('Borrar este control PID?', ['class' => 'btn btn-danger']) !!}
-        {!! Form::close() !!}
-        </p>
-        <hr>
-    @endforeach
 @stop
+
+
+
+
+
+@section('footerspace')
+
+        <!-- jQuery 3 -->
+    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+
+@endsection
