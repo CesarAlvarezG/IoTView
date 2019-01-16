81011626b6b85e704a4a3a45273cb1036ddc70b7
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-01-16 16:49:54 -0500

    Organización de la tabla en medidas

--- resources/views/medida/index.blade.php
+++ resources/views/medida/index.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.app')
 
 
+@section('headspace')
+
+    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+    <script src="{{asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+    <script src="{{asset('bower_components/fastclick/lib/fastclick.js')}}"></script>
+
+    <script src="{{asset('/dist/js/adminlte.min.js')}}"></script>
+    <script src="{{asset('/dist/js/demo.js')}}"></script>
+
+<script>
+  $(function() {
+    $('#tabla1').DataTable()
+
+	})
+</script>
+
+
+
+@endsection
+
+
 @section('sidebar')
 
 
@@ -18,43 +42,58 @@
             <div class="box-header">
                 <h1 class="box-title">Listado de medidas</h1>
                 <p><a href="{!! url('medidas/create') !!}">Agregar una nueva medida?</a></p>
-                <div class="box-body table-responsive no-padding">
-                    <table class="table table-hover">
-                        <tr>
-                            <th>ID</th>
-                            <th>Valor</th>
-                            <th>Sensor</th>
-                            <th>Ver</th>
-                            <th>Editar</th>
-                            <th>Borrar</th>
-                        </tr>
-                        @foreach($list as $medida)
+                <div class="box-body">
+                    <table id="tabla1" class="table table-bordered table-striped">
+                        <thead>
                             <tr>
-                                <td>
-                                    {{ $medida->id }}
-                                </td>
-                                <td>
-                                    {{ $medida->valor }}
-                                </td>
-                                <td>
-                                    {{ $medida->sensor_id }}
-                                </td>
-                                <td>
-                                    <a href="{{ route('medidas.show', $medida->id) }}" class="btn btn-primary">Ver Medida</a>
-                                </td>
-                                <td>
-                                    <a href="{{ route('medidas.edit', $medida->id) }}" class="btn btn-primary">Editar Medida</a>
-                                </td>
-                                <td>
-                                    {!! Form::open([
-                                        'method' => 'DELETE',
-                                        'route' => ['medidas.destroy', $medida->id]
-                                    ]) !!}
-                                    {!! Form::submit('Borrar esta medida?', ['class' => 'btn btn-danger']) !!}
-                                    {!! Form::close() !!}
-                                </td>
+                                <th>ID</th>
+                                <th>Valor</th>
+                                <th>Sensor</th>
+                                <th>Ver</th>
+                                <th>Editar</th>
+                                <th>Borrar</th>
                             </tr>
-                        @endforeach
+                        </thead>
+                        <tbody>
+                            @foreach($list as $medida)
+                                <tr>
+                                    <td>
+                                        {{ $medida->id }}
+                                    </td>
+                                    <td>
+                                        {{ $medida->valor }}
+                                    </td>
+                                    <td>
+                                        {{ $medida->sensor_id }}
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('medidas.show', $medida->id) }}" class="btn btn-primary">Ver Medida</a>
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('medidas.edit', $medida->id) }}" class="btn btn-primary">Editar Medida</a>
+                                    </td>
+                                    <td>
+                                        {!! Form::open([
+                                            'method' => 'DELETE',
+                                            'route' => ['medidas.destroy', $medida->id]
+                                        ]) !!}
+                                        {!! Form::submit('Borrar esta medida?', ['class' => 'btn btn-danger']) !!}
+                                        {!! Form::close() !!}
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <th>ID</th>
+                                <th>Valor</th>
+                                <th>Sensor</th>
+                                <th>Ver</th>
+                                <th>Editar</th>
+                                <th>Borrar</th>
+                            </tr>
+                            </tfoot>
+
                     </table>
                 </div>
             </div>
@@ -68,11 +107,6 @@
 
 @section('footerspace')
 
-        <!-- jQuery 3 -->
-    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
-    <!-- Bootstrap 3.3.7 -->
-    <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{asset('dist/js/adminlte.min.js')}}"></script>
+
 
 @endsection
