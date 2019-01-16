239d0add73e63456b970787838c3616488a5b720
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-01-16 18:03:37 -0500

    Interfaz grafica de crear usuario

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -2,36 +2,47 @@
 
 
 @section('content')
-    <h1>Añadir un nuevo usuario</h1>
-    <p class="lead">.</p>
-    <hr>
-    @if($errors->any())
-        <div class="alert alert-danger">
-            @foreach($errors->all() as $error)
-                <p>{{ $error }}</p>
-            @endforeach
+
+<section class="content">
+    <div class="box box-primary">
+        <div class="box-header with-border">
+             <h1 class="box-title">Añadir un nuevo usuario</h1>
         </div>
-    @endif
+        @if($errors->any())
+            <div class="alert alert-danger">
+                @foreach($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
 
     {!! Form::open(['route' => 'users.store']) !!}
-    <div class="form-group">
-        {!! Form::label('name', 'Nombre', ['class' => 'control-label']) !!}
-        {!! Form::text('name', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('email', 'Email', ['class' => 'control-label']) !!}
-        {!! Form::text('email', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('password', 'Password', ['class' => 'control-label']) !!}
-        {!! Form::password('password', ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('Direccion', 'Dirección', ['class' => 'control-label']) !!}
-        {!! Form::text('Direccion', null, ['class' => 'form-control']) !!}
-    </div>
+        <div class="box-body">
+            <div class="form-group">
+                {!! Form::label('name', 'Nombre', ['class' => 'control-label']) !!}
+                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('email', 'Email', ['class' => 'control-label']) !!}
+                {!! Form::text('email', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('password', 'Password', ['class' => 'control-label']) !!}
+                {!! Form::password('password', ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('Direccion', 'Dirección', ['class' => 'control-label']) !!}
+                {!! Form::text('Direccion', null, ['class' => 'form-control']) !!}
+            </div>
 
-    {!! Form::submit('Crear un nuevo usuario', ['class' => 'btn btn-primary']) !!}
-    {!! Form::close() !!}
+            {!! Form::submit('Crear un nuevo usuario', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+</section>
+
+
+
+
 
 @stop
