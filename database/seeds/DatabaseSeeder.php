2b635c52ed468a649055c05616c5163c5b87031c
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-08-01 18:14:34 -0500

    Actualización de la semilla

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use App\Sistema;
+use App\Sensor;
+use App\Pid;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         DB::table('users')->delete();
         User::create(array('name'=>'Admin','email'=>'admin@correo.com','password'=>'12345678','role'=>'1','Direccion'=>'Armenia'));
+        Sistema::create(array('Nombre'=>'Iot Prueba','Descripcion'=>'Prueba de envio y recepcion de datos por la API','Confirmacion'=>'RphPq81BeT','NVar'=>'VarPrueba','Var'=>'1','NMensaje'=>'Estado','Mensaje'=>'ok'));
+        Sensor::create(array('Nombre'=>'Rampa','Tipo'=>'Sensor','NVar'=>'Dato recibido','Var'=>'0','NMensaje'=>'Estado','Mensaje'=>'ok','sistema_id'=>'1','SetPoint'=>'0'));
     }
 }
