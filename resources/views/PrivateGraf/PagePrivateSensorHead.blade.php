b65d39e8164a3ff8d51f1f9d59d65d1603c6ee26
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2020-04-02 13:18:45 -0500

    Ampliación del rango de medida

--- resources/views/PrivateGraf/PagePrivateSensorHead.blade.php
+++ resources/views/PrivateGraf/PagePrivateSensorHead.blade.php
@@ -68,7 +68,7 @@ var plot{{$sensor->id}} = $.plot("#interactive{{$sensor->id}}", [ getData{{$sens
             },
 			yaxis: {
 				min: 0,
-				max: 3,
+				max: 100,
                 show: true
 			},
 			xaxis: {
